22cb2382a20b305405b2ca5de6f49e828b2b585f
Daniel <tejadadaniel33@gmail.com>
2022-01-12 22:50:57 -0400

    service result used in heroService

--- services/heroService.js
+++ services/heroService.js
@@ -1,32 +1,43 @@
 const Hero = require('../models/heroModel')
+const {success,notFound} = require('../service-result/serviceResult')
 
 
 module.exports.addHero =  (hero) =>{
      const heroModel = new Hero({
          ...hero
      })
-     return heroModel.save()
+     return heroModel.save().then(hero => success(hero))
  }
 
 
 module.exports.getAllHeroes = ()=>{    
-    return Hero.find()
+    return Hero.find().then(heroes => success(heroes))
 }
 
 
 module.exports.getByIdHero =  (id) =>{    
-    return  Hero.findById(id)   
+    return  Hero.findById(id).then(hero => success(hero))   
 }
 
 
 module.exports.deleteHero = (id) =>{
-    return Hero.deleteOne({_id: id})
+    return Hero.deleteOne({_id: id}).then(hero => {
+        if(hero){
+            return success(hero)
+        }
+        return notFound(hero,['Hero not found'])
+    })   
 }
 
 
 module.exports.updateHero = (hero) => {    
     return Hero.findOneAndUpdate({_id: hero.id},hero,{new:true}).populate("user",{
         _id:1
+    }).then(hero => {
+        if(hero){
+            return success(hero)
+        }
+        return notFound(hero,['Hero not found'])
     })
 }
 
